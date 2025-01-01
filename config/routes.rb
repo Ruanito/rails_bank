b30aac415bb16526a17b73f1c982a9ce6eb5ceb5
Ruanito <Ruanito@users.noreply.github.com>
2025-01-01 15:48:22 -0300

    Revert "feature: Add device token"

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
   get "health", to: "health#check"
-
-  devise_for :users, path: "", path_names: {
-    sign_in: "login",
-    sign_out: "logout",
-    registration: "signup"
-  }, controllers: {
-    sessions: "users/sessions",
-    registrations: "users/registrations"
-  }
 end
