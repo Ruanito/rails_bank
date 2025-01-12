12eba16648e7a22f0b474c0d9b6653072023ec1b
Ruanito Santos <ruanitodiego@gmail.com>
2025-01-12 14:54:40 -0300

    feature: Add accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,18 @@
 class AccountsController < ApplicationController
+  before_action :set_user, only: [ :show, :create ]
+  def create
+    account = @current_user.accounts.create({ balance: 0 })
+
+    if account.save
+      render json: account, status: :created
+    else
+      render json: account.errors, status: :bad_request
+    end
+  end
+
+  def show
+    accounts = @current_user.accounts
+
+    render json: accounts, status: :ok
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :name, presence: true
+
+  has_many :accounts, dependent: :destroy
 end
